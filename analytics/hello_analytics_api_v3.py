8ff7ffb341fc70356c145454a31117a88c07cde6
munkav <vishalmunka@gmail.com>
2014-12-06 20:21:02 -0500

    adding analytics changes

--- analytics/hello_analytics_api_v3.py
+++ analytics/hello_analytics_api_v3.py
@@ -26,7 +26,8 @@ def main(argv):
             results = get_results(service, profile_id)
 
             print profile_id
-            # Step 4. Output the results.
+            # Step 4. Output the results. 
+            print_results(results)
             process_results(results)
 
     except TypeError, error:
@@ -72,13 +73,14 @@ def get_first_profile_id(service):
 
 
 def get_results(service, profile_id):
+    profile_id = "94808633"
     # Use the Analytics Service Object to query the Core Reporting API
     return service.data().ga().get(
         ids='ga:' + profile_id,
         dimensions='ga:eventCategory,ga:eventAction,ga:eventLabel',
         metrics='ga:totalEvents',
         start_date='2014-12-04',
-        end_date='2014-12-05').execute()
+        end_date='2014-12-06').execute()
 
 
 def print_results(results):
@@ -102,33 +104,43 @@ def process_results(results):
     if results.get('rows', []):
         for row in results.get('rows'):
             # splitting the lable on ? and then on &s
-            time, userReadData = row[2].split('?')
-            time = time.strip()
-            userReadData = parser.unescape(userReadData)
+	    print 'before split ' + row[2]
+	    try :
+            	timeSeconds, userReadData = row[2].split('?')
+            	userReadData = parser.unescape(userReadData)
+	    except ValueError :
+                 print "invalid label "  #+ row[2]
+                 continue
             if userReadData:
                 try:
                     article = userReadData.split('&')[0].split('=')[1]
                     section = userReadData.split('&')[1].split('=')[1]
                     userId = userReadData.split('&')[2].split('=')[1].encode('utf-8')
+		    expectedTime = userReadData.split('&')[3].split('=')[1]
                 except (ValueError, IndexError) as e:
                     print "invalid label "  #+ row[2]
                     continue
+	   	    
+	    if not expectedTime :
+		continue
+	    time = float(timeSeconds.strip())/float(expectedTime)
 
             #Finding maximum time spent on each article and saving the corresponding time and section
             if userDataMap.get(userId, {}).has_key(article):
                 oldData = userDataMap[userId][article]
-                if (int(oldData[0]) < int(time)):
-                    userDataMap[userId][article] = [int(time), section]
+                if (float(oldData[0]) < float(time)):
+                    userDataMap[userId][article] = [time, section]
             else:
-                userDataMap.setdefault(userId, {})[article] = [int(time), section]
+                userDataMap.setdefault(userId, {})[article] = [time, section]
 
         print userDataMap.items()
     else:
         print 'No Rows Found'
-
+    
+    #userDataMap['vishal']['asdf']= [10, 'travel.america']
     createUserProfileUpdateData(userDataMap)
 
-
+'''
 def createUserProfileUpdateData(userDataMap):
     # clubbing time for each section for each user
     userProfileMap = {}
@@ -142,6 +154,32 @@ def createUserProfileUpdateData(userDataMap):
     logging.info(userProfileMap.items())
 
     updateUserDatabase(userProfileMap)
+'''
+
+def createUserProfileUpdateData(userDataMap):
+    # clubbing time for each section for each user
+    userProfileMap = {}
+    for user in userDataMap:
+        for article in userDataMap[user]:
+            time, section = userDataMap[user][article]
+            if "." in section :
+		sectionKey, subsectionKey = section.split('.') 		
+		if userProfileMap.get(user, {}).has_key(section):
+                    userProfileMap[user][section] = userProfileMap[user][section] + time
+                else:
+                    userProfileMap.setdefault(user, {})[section] = time
+		if userProfileMap.get(user, {}).has_key(sectionKey):
+		    userProfileMap[user][sectionKey] = userProfileMap[user][sectionKey] + time
+		else:
+		    userProfileMap.setdefault(user, {})[sectionKey] = time
+            else:
+                if userProfileMap.get(user, {}).has_key(section):
+                    userProfileMap[user][section] = userProfileMap[user][section] + time
+                else:
+                    userProfileMap.setdefault(user, {})[section] = time
+    print userProfileMap.items()
+
+    updateUserDatabase(userProfileMap)
 
 
 def updateUserDatabase(userProfileMap):
@@ -152,12 +190,25 @@ def updateUserDatabase(userProfileMap):
         u = users.find_one({"_id": user})
         totalTime = 0
         for section in userProfileMap[user]:
-            clicks, time = u['categories'][section]
-            print "clicks " + str(clicks) + "time " + str(time)
-            users.update({'_id': user},
-                         {'$set': {'categories.' + section: [clicks, time + userProfileMap[user][section]]}})
-            totalTime = totalTime + userProfileMap[user][section]
-        users.update({'_id': user}, {'$inc': {'totalTime': totalTime}})
+	    totalTime = totalTime + userProfileMap[user][section]
+	    if "." in section :
+		    sectionKey, subSectionKey = section.split('.')
+		    clicksSubSection, timeSubSection = u['categories'][sectionKey][subSectionKey]
+		    #clicksSection, timeSection = u['categories'][sectionKey]['total']
+		    print "clicks " + str(clicksSubSection) + "time " + str(timeSubSection)
+		    users.update({'_id': user},
+		                 {'$set': {'categories.' + sectionKey +"." + subSectionKey: [clicksSubSection, timeSubSection + userProfileMap[user][section]]}})			
+	    else :
+	    	if isinstance(u['categories'][section], dict) :
+		    clicksSection, timeSection = u['categories'][section]['total']
+		    users.update({'_id': user},
+		                 {'$set': {'categories.' + section +"." + 'total': [clicksSection, timeSection + userProfileMap[user][section]]}})	
+	    	else :
+		    clicksSection, timeSection = u['categories'][section]
+		    users.update({'_id': user},
+		                 {'$set': {'categories.' + section : [clicksSection, timeSection + userProfileMap[user][section]]}})	
+	
+	users.update({'_id': user}, {'$inc': {'totalTime': totalTime}})
 
 
 if __name__ == '__main__':
