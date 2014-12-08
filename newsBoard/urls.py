0a05613ddd82ac8a5b93caea46ab577f32ddd5a2
Ubuntu <ubuntu@ip-172-31-20-204.us-west-2.compute.internal>
2014-12-08 02:28:53 +0000

    addding deleted urls.py

--- newsBoard/urls.py
+++ newsBoard/urls.py
@@ -1,18 +1,13 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
-
-from userHomePage.views import home, news, article, section
+from userHomePage.views import home, news, article
 admin.autodiscover()
-
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'newsBoard.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', home),
-    url(r'^news', news),
-    url(r'^article', article),
-    url(r'^section', section)
+# Examples:
+# url(r'^$', 'newsBoard.views.home', name='home'),
+# url(r'^blog/', include('blog.urls')),
+url(r'^admin/', include(admin.site.urls)),
+url(r'^$', home),
+url(r'^news/', news),
+url(r'^article', article)
 )
